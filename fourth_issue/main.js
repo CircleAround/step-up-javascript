2855126f75597b24fcfb91296aa19e0cd3774159
kabane <kabaneshi@gmail.com>
2021-04-16 05:53:47 +0900

    書籍に合わせたコード修正

--- fourth_issue/main.js
+++ fourth_issue/main.js
@@ -1,24 +1,32 @@
-const rootElm = document.getElementById('areaSelector')
+const rootElm = document.getElementById('areaSelector');
 
-async function init() {
+async function initAreaSelector() {
   await updatePref();
   await updateCity();
 }
 
 async function getPrefs() {
-  const prefResponse = await fetch('./prefectures.json');
-  return await prefResponse.json();
+  try {
+    const prefResponse = await fetch('./prefectures.json');
+    return await prefResponse.json();
+  } catch(e) {
+    console.log('都道府県の情報取得に失敗しました');
+  }
 }
 
 async function getCities(cityCode) {
-  const cityResponse = await fetch(`./cities/${cityCode}.json`);
-  return await cityResponse.json();
+  try {
+    const cityResponse = await fetch(`./cities/${cityCode}.json`);
+    return await cityResponse.json();
+  } catch(e) {
+    console.log('市区町村の情報取得に失敗しました');
+  }
 }
 
 async function updatePref() {
   const prefs = await getPrefs();
   createPrefOptionsHtml(prefs);
-}
+} 
 
 async function updateCity() {
   const prefSelectorElm = rootElm.querySelector('.prefectures');
@@ -39,10 +47,12 @@ async function createPrefOptionsHtml(prefs) {
 
   const prefSelectorElm = rootElm.querySelector('.prefectures');
   prefSelectorElm.innerHTML = optionStrs.join('\n');
+
   prefSelectorElm.addEventListener('change', async(event) => {
     updateCity(event.target.value);
   });
-}
+} 
+
 
 function createCityOptionsHtml(cities) {
   const optionStrs = [];
@@ -54,9 +64,11 @@ function createCityOptionsHtml(cities) {
       </option>
     `);
   }
-    
+      
   const citySelectorElm = rootElm.querySelector('.cities');
   citySelectorElm.innerHTML = optionStrs.join('\n');
+
+;
 }
 
-init();
+initAreaSelector();
