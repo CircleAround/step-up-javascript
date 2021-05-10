908017b7ea284c51f11c12aaa4de40273fe104db
kabane <kabaneshi@gmail.com>
2021-05-10 16:19:28 +0900

    書籍に合わせてコードを変更

--- final_issue/main.js
+++ final_issue/main.js
@@ -1,18 +1,7 @@
 class WordQuiz {
   constructor(rootElm) {
     this.rootElm = rootElm;
-    this.statusValues = {
-      ready: 0,
-      playing: 1,
-      done: 2
-    };
     this.init();
-  }
-
-  async init() {
-    this.resetGame();
-    await this.fetchQuizData();
-    await this.updateHtml();
   }
 
   async fetchQuizData() {
@@ -21,39 +10,33 @@ class WordQuiz {
       this.quizData = await response.json();
     } catch (e) {
       this.rootElm.innerText = '問題の読み込みに失敗しました';
-      console.log(e);
-    } 
+      console.error(e);
+    }
   }
 
-  startGame() {
-    this.status = this.statusValues.playing;
-    this.gameStatus.step = 1;
-    this.gameStatus.timeLimit = 10;
-    this.updateHtml();
-    this.gameStatus.intervalKey = setInterval(() => this.handleTimeLimit(), 1000);
-  }
-
-  resetGame() {
-    this.gameStatus = {
-      level: null,
-      step: null,
-      timeLimit: 0,
-      intervalKey: null,
-      score: 0,
-      results: []
+  async init() {
+    // 画面のステータス
+    this.statusValues = {
+      ready: 0,  // ゲーム開始可能
+      playing: 1, // ゲームプレイ中
+      done: 2 // ゲーム終了
     };
-    this.status = this.statusValues.ready;
+    // ゲームのステータス
+    this.gameStatus = {
+      level: null, // 選択されたレベル
+      step: 1, // 現在表示している問題の番
+      results: [], // プレイヤーの回答結果
+      timeLimit: 0, // 問題毎の制限時間
+      intervalKey: null,
+    };
+    await this.fetchQuizData();
+    this.changeStatus('ready');
+    this.updateHtml();
   }
 
-  addResult(answer) {
-    this.gameStatus.results.push({
-      question: this.getCurrentQuestion(),
-      selectedAnswer: answer
-    });
-  }
-
-  getCurrentQuestion() {
-    return this.quizData[this.gameStatus.level][`step${this.gameStatus.step}`];
+  changeStatus(status) {
+    // ゲーム開始前のステータス設定
+    this.status = this.statusValues[status];
   }
 
   isLastStep() {
@@ -61,52 +44,68 @@ class WordQuiz {
     return this.gameStatus.step === Object.keys(currentQuestions).length;
   }
 
-  isPlaying() {
-    return this.status === this.statusValues.playing;
+  resetGame() {
+    this.gameStatus.level = null;
+    this.gameStatus.step = 1;
+    this.gameStatus.results = [];
+    this.gameStatus.timeLimit = 0;
+    this.gameStatus.intervalKey = null;
   }
 
-  initDataByStep() {
-    this.resetIntervalKey();
-    if (this.isLastStep()) {
-      this.gameStatus.step = null;
-      this.status = this.statusValues.done;
-      this.calcScore();
-    } else {
-      this.gameStatus.intervalKey = setInterval(() => this.handleTimeLimit(), 1000);
-      this.gameStatus.step++;
-      this.gameStatus.timeLimit = 10;
-    }
-  }
-
-  nextStep() {
-    if(this.isPlaying()) {
-      const checkedElm = this.rootElm.querySelector('input[name="choice"]:checked');
-      this.addResult(checkedElm ? checkedElm.value : '');
-    }
-
-    this.initDataByStep();
-    this.updateHtml();
-  }
-
-  handleTimeLimit() {
-    this.gameStatus.timeLimit--;
-    if (this.gameStatus.timeLimit === 0) {
-      this.nextStep();
-    } else {
-      this.updateTimeLimitStr();
-    }
+  setTimer() {
+    this.gameStatus.intervalKey = setInterval(() => {
+      this.gameStatus.timeLimit--;
+      if (this.gameStatus.timeLimit === 0) {
+        this.nextStep();
+      } else {
+        this.renderTimeLimitStr();
+      }
+    }, 1000);
   }
 
   resetIntervalKey() {
     this.gameStatus.intervalKey = clearInterval(this.gameStatus.intervalKey);
   }
 
-  calcScore() {
-    const correctQuestionNum = this.gameStatus.results.filter((result)=>{
-      return result.selectedAnswer === result.question.answer;
-    }).length;
+  addResult() {
+    const checkedElm = this.rootElm.querySelector('input[name="choice"]:checked');
+    const answer = checkedElm ? checkedElm.value : '';
+    const currentQuestions = this.quizData[this.gameStatus.level][`step${this.gameStatus.step}`];
 
-    this.gameStatus.score = Math.floor((correctQuestionNum / this.gameStatus.results.length) * 100);
+    this.gameStatus.results.push({
+      question: currentQuestions,
+      selectedAnswer: answer
+    });
+
+    console.log(`解答結果: ${answer}`)
+  }
+
+  nextStep() {
+    this.resetIntervalKey();
+    this.addResult();
+
+    if (this.isLastStep()) {
+      this.changeStatus('done');
+    } else {
+      this.gameStatus.step++;
+    }
+
+    this.updateHtml();
+  }
+
+  calcScore() {
+    let correctNum = 0
+    const results = this.gameStatus.results;
+
+    for(let i = 0; results.length > i; i++) {
+      const result = results[i];
+      const selected = result.selectedAnswer;
+      const correct = result.question.answer;
+
+      correctNum += selected === correct ? 1 : 0;
+    }
+
+    return Math.floor((correctNum / results.length) * 100);
   }
 
   updateHtml() {
@@ -125,12 +124,13 @@ class WordQuiz {
     }
   }
 
-  createStartHtml() { 
+  createStartHtml() {
     const levelsStr = Object.keys(this.quizData);
     this.gameStatus.level = levelsStr[0];
-    const optionsStr = levelsStr.map((level, index) => {
-      return `<option value="${level}" name="level">レベル${index + 1}</option>`;
-    });
+    const optionsStr = [];
+    for(let i = 0; levelsStr.length > i; i++) {
+      optionsStr.push(`<option value="${levelsStr[i]}" name="level">レベル${i + 1}</option>`);
+    }
 
     const template = `
       <select class="levelSelector">
@@ -149,28 +149,35 @@ class WordQuiz {
 
     const startBtnElm = parentElm.querySelector('.startBtn');
     startBtnElm.addEventListener('click', () => {
-      this.startGame();
+      this.changeStatus('playing');
+      this.updateHtml();
     });
 
     return parentElm;
   }
 
   createQuetionHtml() {
-    const currentQuestion = this.getCurrentQuestion();
-    const answerGroup = currentQuestion.choices.map((choice) => {
-      return `<label>
-                <input type="radio" name="choice" value="${choice}" />
-                ${choice}
-              </label>`;
-    });
-    let template = `
+    this.gameStatus.timeLimit = 10;
+    console.log(`選択中のレベル:${this.gameStatus.level}`);
+    const currentQuestion = this.quizData[this.gameStatus.level][`step${this.gameStatus.step}`];
+    const answerGroup = [];
+    const choices = currentQuestion.choices;
+
+    for(let i = 0; choices.length > i; i++) {
+      answerGroup.push(`<label>
+                          <input type="radio" name="choice" value="${choices[i]}" />
+                          ${choices[i]}
+                        </label>`);
+    }
+
+    const template = `
       <p class="alertMessage"></p>
       <p>${currentQuestion.word}</p>
       <div>
         ${answerGroup.join('\n')}
       </div>
       <button class="nextBtn">回答する</button>
-      <p class="sec">${this.timeLimitStr()}</p>
+      <p class="sec">残り回答時間:${this.gameStatus.timeLimit}秒</p>
     `;
 
     const parentElm = document.createElement('div');
@@ -182,12 +189,21 @@ class WordQuiz {
       this.nextStep();
     });
 
+    this.setTimer();
     return parentElm;
   }
 
+  renderTimeLimitStr() {
+    let secElm = this.rootElm.querySelector('.sec');
+    secElm.innerText = `残り回答時間:${this.gameStatus.timeLimit}秒`;
+  }
+
   createResultsHtml() {
+    const score = this.calcScore();
+
     const template = `
-      <p>正解率${this.gameStatus.score}%</p>
+      <h2>集計結果</h2>
+      <p>正解率: ${score}%</p>
       <button class="resetBtn">開始画面に戻る</button>
     `;
 
@@ -197,24 +213,14 @@ class WordQuiz {
 
     const resetBtnElm = parentElm.querySelector('.resetBtn');
     resetBtnElm.addEventListener('click', () => {
+      this.changeStatus('ready');
       this.resetGame();
       this.updateHtml();
     });
 
     return parentElm;
   }
-
-  timeLimitStr() {
-    return `残り回答時間:${this.gameStatus.timeLimit}秒`;
-  }
-
-  updateTimeLimitStr() {
-    let secElm = this.rootElm.querySelector('.sec');
-    if (!secElm) return;
-
-    secElm.innerText = this.timeLimitStr();
-  }
-
+  
 }
 
 new WordQuiz(document.getElementById('app'));
