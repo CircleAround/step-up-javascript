41979792d1d2df2a2844c0fabde9d3b7353521da
ms2sato <ms2.sato@gmail.com>
2021-06-08 11:56:36 +0900

    change step numbers

--- step6/main.js
+++ step6/main.js
@@ -1,199 +1,72 @@
-class WordQuiz {
-  constructor(rootElm) {
-    this.rootElm = rootElm;
+const rootElm = document.getElementById('areaSelector');
 
-    // ゲームのステータス
-    this.gameStatus = {};
-    this.resetGame();
-  }
+async function initAreaSelector() {
+  await updatePref();
+  await updateCity();
+}
 
-  async init() {
-    await this.fetchQuizData();
-    this.displayStartView();
-  }
-
-  async fetchQuizData() {
-    try {
-      const response = await fetch('quiz.json');
-      this.quizData = await response.json();
-    } catch (e) {
-      this.rootElm.innerText = '問題の読み込みに失敗しました';
-      console.log(e);
-    }
-  }
-
-  isLastStep() {
-    const currentQuestions = this.quizData[this.gameStatus.level];
-    return this.gameStatus.step === Object.keys(currentQuestions).length;
-  }
-
-  nextStep() {
-    this.clearTimer();
-    this.addResult();
-
-    if (this.isLastStep()) {
-      this.displayResultView();
-    } else {
-      this.gameStatus.step++;
-      this.displayQuestionView();
-    }
-  }
-
-  addResult() {
-    const checkedElm = this.rootElm.querySelector('input[name="choice"]:checked');
-    const answer = checkedElm ? checkedElm.value : '';
-    const currentQuestion = this.quizData[this.gameStatus.level][`step${this.gameStatus.step}`];
-
-    this.gameStatus.results.push({
-      question: currentQuestion,
-      selectedAnswer: answer
-    });
-
-    console.log(`解答結果: ${answer}`);
-  }
-
-  calcScore() {
-    let correctNum = 0;
-    const results = this.gameStatus.results;
-
-    for (const result of results) {
-      const selected = result.selectedAnswer;
-      const correct = result.question.answer;
-
-      if (selected === correct) {
-        correctNum++;
-      }
-    }
-
-    return Math.floor((correctNum / results.length) * 100);
-  }
-
-  resetGame() {
-    this.gameStatus.level = null; // 選択されたレベル
-    this.gameStatus.step = 1; // 現在表示している設問の番号
-    this.gameStatus.results = []; // プレイヤーの解答結果
-    this.gameStatus.timeLimit = 0; // 問題毎の制限時間
-    this.gameStatus.intervalKey = null; // setIntervalのキー
-  }
-
-  setTimer() {
-    this.gameStatus.timeLimit = 10;
-
-    this.gameStatus.intervalKey = setInterval(() => {
-      this.gameStatus.timeLimit--;
-      if (this.gameStatus.timeLimit === 0) {
-        this.nextStep();
-      } else {
-        this.renderTimeLimitStr();
-      }
-    }, 1000);
-  }
-
-  clearTimer() {
-    clearInterval(this.gameStatus.intervalKey);
-    this.gameStatus.intervalKey = null;
-  }
-
-  displayStartView() {
-    const levelStrs = Object.keys(this.quizData);
-    this.gameStatus.level = levelStrs[0];
-
-    const optionStrs = [];
-    for (let i = 0; levelStrs.length > i; i++) {
-      optionStrs.push(`<option value="${levelStrs[i]}" name="level">レベル${i + 1}</option>`);
-    }
-
-    const html = `
-      <select class="levelSelector">
-        ${optionStrs.join('\n')}
-      </select>
-      <button class='startBtn'>スタート</button>
-    `;
-    const parentElm = document.createElement('div');
-    parentElm.innerHTML = html;
-
-    const selectorElm = parentElm.querySelector('.levelSelector');
-    selectorElm.addEventListener('change', (event) => {
-      this.gameStatus.level = event.target.value;
-    });
-
-    const startBtnElm = parentElm.querySelector('.startBtn');
-    startBtnElm.addEventListener('click', () => {
-      this.displayQuestionView();
-    });
-
-    this.replaceView(parentElm);
-  }
-
-  displayQuestionView() {
-    console.log(`選択中のレベル:${this.gameStatus.level}`);
-    this.setTimer();
-
-    const stepKey = `step${this.gameStatus.step}`;
-    const currentQuestion = this.quizData[this.gameStatus.level][stepKey];
-
-    const choiceStrs = [];
-    for (const choice of currentQuestion.choices) {
-      choiceStrs.push(`<label>
-                          <input type="radio" name="choice" value="${choice}" />
-                          ${choice}
-                        </label>`);
-    }
-
-    const html = ` 
-      <p>${currentQuestion.word}</p>
-      <div>
-        ${choiceStrs.join('\n')}
-      </div>
-      <div class="actions">
-        <button class="nextBtn">解答する</button>
-      </div>
-      <p class="sec">残り解答時間:${this.gameStatus.timeLimit}秒</p>
-    `;
-
-    const parentElm = document.createElement('div');
-    parentElm.className = 'question';
-    parentElm.innerHTML = html;
-
-    const nextBtnElm = parentElm.querySelector('.nextBtn');
-    nextBtnElm.addEventListener('click', () => {
-      this.nextStep();
-    });
-
-    this.replaceView(parentElm);
-  }
-
-  renderTimeLimitStr() {
-    const secElm = this.rootElm.querySelector('.sec');
-    secElm.innerText = `残り解答時間:${this.gameStatus.timeLimit}秒`;
-  }
-
-  displayResultView() {
-    const score = this.calcScore();
-
-    const html = `
-      <h2>ゲーム終了</h2>
-      <p>正解率: ${score}%</p>
-      <button class="resetBtn">開始画面に戻る</button>
-    `;
-
-    const parentElm = document.createElement('div');
-    parentElm.className = 'results';
-    parentElm.innerHTML = html;
-
-    const resetBtnElm = parentElm.querySelector('.resetBtn');
-    resetBtnElm.addEventListener('click', () => {
-      this.resetGame();
-      this.displayStartView();
-    });
-
-    this.replaceView(parentElm);
-  }
-
-  replaceView(elm) {
-    this.rootElm.innerHTML = '';
-    this.rootElm.appendChild(elm);
+async function getPrefs() {
+  try {
+    const prefResponse = await fetch('./prefectures.json');
+    return await prefResponse.json();
+  } catch(e) {
+    console.log('都道府県の情報取得に失敗しました');
   }
 }
 
-new WordQuiz(document.getElementById('app')).init();
+async function getCities(cityCode) {
+  try {
+    const cityResponse = await fetch(`./cities/${cityCode}.json`);
+    return await cityResponse.json();
+  } catch(e) {
+    console.log('市区町村の情報取得に失敗しました');
+  }
+}
+
+async function updatePref() {
+  const prefs = await getPrefs();
+  createPrefOptionsHtml(prefs);
+} 
+
+async function updateCity() {
+  const prefSelectorElm = rootElm.querySelector('.prefectures');
+  const cities = await getCities(prefSelectorElm.value);
+  createCityOptionsHtml(cities);
+}
+
+async function createPrefOptionsHtml(prefs) {
+  const optionStrs = [];
+  for(let i = 0; prefs.length > i; i++) {
+    const pref = prefs[i];
+    optionStrs.push(`
+      <option name="${pref.name}" value="${pref.code}">
+        ${pref.name}
+      </option>
+    `);
+  }
+
+  const prefSelectorElm = rootElm.querySelector('.prefectures');
+  prefSelectorElm.innerHTML = optionStrs.join('\n');
+
+  prefSelectorElm.addEventListener('change', async(event) => {
+    updateCity(event.target.value);
+  });
+} 
+
+
+function createCityOptionsHtml(cities) {
+  const optionStrs = [];
+  for(let i = 0; cities.length > i; i++) {
+    const city = cities[i];
+    optionStrs.push(`
+      <option name="${city.name}" value="${city.code}">
+        ${city.name}
+      </option>
+    `);
+  }
+      
+  const citySelectorElm = rootElm.querySelector('.cities');
+  citySelectorElm.innerHTML = optionStrs.join('\n');
+}
+
+initAreaSelector();
