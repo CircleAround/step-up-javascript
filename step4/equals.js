4ecf18e422c5f7991e7438a858b751da8beb5868
Masashi Sato <ms2.sato@gmail.com>
2021-06-16 16:14:05 +0900

    step4 等価演算子のサンプル

---
+++ step4/equals.js
@@ -0,0 +1,10 @@
+console.log("3" == 3); // => true
+console.log("3" === 3); // => false
+
+const test1 = { message: 'hello' };
+const test2 = { message: 'hello' };
+const test3 = test1;
+console.log(test1 == test2); // => false
+console.log(test1 === test2); // => false
+console.log(test1 == test3); // => true
+console.log(test1 === test3); // => true
