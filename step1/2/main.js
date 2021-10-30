395484cf261b9787ada0ed2057577416f10daac4
Masashi Sato <ms2.sato@gmail.com>
2021-10-30 14:30:39 +0900

    step1 multiline comment

--- step1/2/main.js
+++ step1/2/main.js
@@ -1,4 +1,5 @@
-// startButtonというclassが付いているタグ要素のうち、最初のもの（スタートボタン）を取り出す
+// startButtonというclassが付いているタグ要素のうち、
+// 最初のもの（スタートボタン）を取り出す
 var startButton = document.getElementsByClassName('startButton')[0]; // --- [1]
 
 // 取り出したstartButtonに対してクリックイベントのリスナを仕掛ける
